5113117af8ab4e9fec1d50f6e795105f7e557dda
Chase Childers <chasechilders12@gmail.com>
2022-07-14 16:13:27 -0400

    added nav bar

--- client/src/components/PrimaryNav.js
+++ client/src/components/PrimaryNav.js
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom'
+import './PrimaryNav.css'
 
 function PrimaryNav() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </nav>
+    <div>
+      <div className="navbar">
+        <img className="logo" src="/images/logo.png" alt="logo"></img>
+        <nav className="links">
+          <Link className="link" to="/">
+            Dashboard
+          </Link>
+          <Link className="link" to="/about">
+            About Us
+          </Link>
+        </nav>
+      </div>
+    </div>
   )
 }
 
