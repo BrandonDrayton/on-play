ab4e720ee1228ff535b334662f8551e522c2a637
Kyle Huskey <kylehuskey@gmail.com>
2022-07-15 15:35:19 -0400

    added multi leagues and teams

--- client/src/services/createSportsApi.js
+++ client/src/services/createSportsApi.js
@@ -3,11 +3,32 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const createSportsApi = createApi({
   reducerPath: 'sportsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://site.api.espn.com/apis/site/v2/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://site.api.espn.com/apis/site/v2/sports/' }),
   endpoints: (builder) => ({
-    getTeams: builder.query({
-      query: () => 'sports/football/nfl/teams',
+    getNflTeams: builder.query({
+      query: () => `football/nfl/teams`,
+    }),
+    getMlbTeams: builder.query({
+      query: () => `baseball/mlb/teams`,
+    }),
+    getNhlTeams: builder.query({
+      query: () => `hockey/nhl/teams`,
+    }),
+    getMlsTeams: builder.query({
+      query: () => `soccer/mls/teams`,
+    }),
+    //   getPgaTeams: builder.query({
+    //     query: () => `golf/leagues/pga/playe`,
+    //   }),
+    getNbaTeams: builder.query({
+      query: () => `basketball/nba/teams`,
     }),
   }),
 })
-export const { useGetTeamsQuery } = createSportsApi
+export const {
+  useGetNflTeamsQuery,
+  useGetMlbTeamsQuery,
+  useGetNhlTeamsQuery,
+  useGetMlsTeamsQuery,
+  useGetNbaTeamsQuery,
+} = createSportsApi
