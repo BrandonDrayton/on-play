e589716f0798328ec1afd435af9683eb2207ebf5
Chase Childers <chasechilders12@gmail.com>
2022-07-26 10:50:55 -0400

    fixed bug

--- client/src/components/ForumModel.jsx
+++ client/src/components/ForumModel.jsx
@@ -203,26 +203,59 @@ function ForumModel() {
                           {createdAtTime}
                         </Text>
                       </Flex>
-                    </AccordionButton>
-                  </h2>
-                  <AccordionPanel display="flex" flexDirection="column" pb={4}>
-                    <Button className="thread-comment-button" mt={3} onClick={() => onCommentClick(null)} bg="#66CD00">
-                      <ChatIcon mr="2"></ChatIcon>
-                      <Text>Comment</Text>
-                    </Button>
-                    {threadData?.Comments?.map((Comment) => {
-                      return (
-                        <Box
-                          onClick={() => setOpenComment(Comment.id)}
-                          key={Comment.id}
-                          boxShadow="md"
-                          p="6"
-                          rounded="md"
-                          bg="white"
-                        >
-                          <Flex flexDirection="column">
-                            <Flex align="center" mb="3">
-                              <Avatar></Avatar>
+                    </Flex>
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel display="flex" flexDirection="column" pb={4}>
+                  <Button className="thread-comment-button" mt={3} onClick={() => onCommentClick(null)} bg="#66CD00">
+                    <ChatIcon mr="2"></ChatIcon>
+                    <Text>Comment</Text>
+                  </Button>
+                  {threadData?.Comments?.map((Comment) => {
+                    return (
+                      <Box
+                        onClick={() => setOpenComment(Comment.id)}
+                        key={Comment.id}
+                        boxShadow="md"
+                        p="6"
+                        rounded="md"
+                        bg="white"
+                      >
+                        <Flex flexDirection="column">
+                          <Flex align="center" mb="3">
+                            <Avatar></Avatar>
+                            <Flex>
+                              <ChatIcon
+                                className="icons"
+                                mr="2"
+                                w="4"
+                                height="6"
+                                onClick={() => onCommentClick(Comment.id)}
+                              ></ChatIcon>
+                              <Text mr="2">{Comment.Children?.length ?? 0}</Text>
+                            </Flex>
+                            <Flex justify="center">
+                              <WrapItem>
+                                <Button
+                                  isDisabled={Comment.Likes.some((l) => l.UserId === userData.id)}
+                                  isActive={Comment.Likes.some((l) => l.UserId !== userData.id)}
+                                  onClick={() => handleAddLike(openThread, Comment.id)}
+                                  colorScheme="whatsapp"
+                                >
+                                  <ArrowUpIcon className="icons" mr="2" w="5" height="6"></ArrowUpIcon>
+                                  <Text mr="2">{Comment.Likes.length}</Text>
+                                </Button>
+                              </WrapItem>
+                              <Text ml="3" fontSize="l">
+                                Chase Childers
+                              </Text>
+                            </Flex>
+                          </Flex>
+                          <Box>
+                            <Text>{Comment.body}</Text>
+                          </Box>
+                          <Flex justify="flex-end" align={'center'}>
+                            <Flex align="center" mt="2" mr="2">
                               <Flex>
                                 <ChatIcon
                                   className="icons"
@@ -234,167 +267,134 @@ function ForumModel() {
                                 <Text mr="2">{Comment.Children?.length ?? 0}</Text>
                               </Flex>
                               <Flex justify="center">
-                                <WrapItem>
-                                  <Button
-                                    isDisabled={Comment.Likes.some((l) => l.UserId === userData.id)}
-                                    isActive={Comment.Likes.some((l) => l.UserId !== userData.id)}
-                                    onClick={() => handleAddLike(openThread, Comment.id)}
-                                    colorScheme="whatsapp"
-                                  >
-                                    <ArrowUpIcon className="icons" mr="2" w="5" height="6"></ArrowUpIcon>
-                                    <Text mr="2">{Comment.Likes.length}</Text>
-                                  </Button>
-                                </WrapItem>
-                                <Text ml="3" fontSize="l">
-                                  Chase Childers
-                                </Text>
+                                <ArrowUpIcon className="icons" mr="2" w="5" height="6"></ArrowUpIcon>
+                                <Text mr="2">{Comment.Likes?.length ?? 0}</Text>
                               </Flex>
                             </Flex>
                             <Box>
-                              <Text>{Comment.body}</Text>
+                              <Text mt="2">
+                                {subCommentTime} {createdAtTime}
+                              </Text>
                             </Box>
-                            <Flex justify="flex-end" align={'center'}>
-                              <Flex align="center" mt="2" mr="2">
-                                <Flex>
-                                  <ChatIcon
-                                    className="icons"
-                                    mr="2"
-                                    w="4"
-                                    height="6"
-                                    onClick={() => onCommentClick(Comment.id)}
-                                  ></ChatIcon>
-                                  <Text mr="2">{Comment.Children?.length ?? 0}</Text>
-                                </Flex>
-                                <Flex justify="center">
-                                  <ArrowUpIcon className="icons" mr="2" w="5" height="6"></ArrowUpIcon>
-                                  <Text mr="2">{Comment.Likes?.length ?? 0}</Text>
-                                </Flex>
-                              </Flex>
-                              <Box>
-                                <Text mt="2">
-                                  {subCommentTime} {createdAtTime}
-                                </Text>
-                              </Box>
-                            </Flex>
                           </Flex>
-                          {Comment.Children.map((Child) => {
-                            return (
-                              <Flex
-                                boxShadow="md"
-                                p="6"
-                                rounded="md"
-                                bg="white"
-                                mt="3"
-                                w="70%"
-                                align="flex-end"
-                                key={Child.id}
-                              >
-                                <Flex flexDirection="column">
-                                  <Flex align="center" mb="3">
-                                    <Avatar></Avatar>
-                                    <Flex>
-                                      <Text ml="3" fontSize="l">
-                                        sdkjhfbkdbfksdf
-                                      </Text>
-                                    </Flex>
-                                  </Flex>
-                                  <Flex align="flex-end">
-                                    <Text>{Child.body}</Text>
-                                  </Flex>
-                                  <Flex justify="flex-end" align={'center'}>
-                                    <Box>
-                                      <Text mt="2">{Child.createdAt}</Text>
-                                    </Box>
+                        </Flex>
+                        {Comment.Children.map((Child) => {
+                          return (
+                            <Flex
+                              boxShadow="md"
+                              p="6"
+                              rounded="md"
+                              bg="white"
+                              mt="3"
+                              w="70%"
+                              align="flex-end"
+                              key={Child.id}
+                            >
+                              <Flex flexDirection="column">
+                                <Flex align="center" mb="3">
+                                  <Avatar></Avatar>
+                                  <Flex>
+                                    <Text ml="3" fontSize="l">
+                                      sdkjhfbkdbfksdf
+                                    </Text>
                                   </Flex>
                                 </Flex>
+                                <Flex align="flex-end">
+                                  <Text>{Child.body}</Text>
+                                </Flex>
+                                <Flex justify="flex-end" align={'center'}>
+                                  <Box>
+                                    <Text mt="2">{Child.createdAt}</Text>
+                                  </Box>
+                                </Flex>
                               </Flex>
-                            )
-                          })}
-                        </Box>
-                      )
-                    })}
-                  </AccordionPanel>
-                </AccordionItem>
-              )
-            })}
-        </Accordion>
-        <Modal size="xl" isOpen={isOpen} onClose={onClose} isCentered>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>
-              <Flex flexDirection="column">
-                <Box>
-                  <Text>ESPN Just Gave Justin Fields Even More Reasons To Crush 2022</Text>
-                </Box>
-                <Box>
-                  <Text fontSize="sm" mt="3">
-                    Aug 13th, 2019, 10:08 AM
-                  </Text>
-                </Box>
-              </Flex>
-            </ModalHeader>
-            <ModalCloseButton />
-            <form onSubmit={handleAddComment}>
-              <ModalBody>
-                <Textarea
-                  value={formComment.body}
-                  height={250}
-                  placeholder="Type comment here"
-                  fontFamily="Poppins"
-                  fontSize="20px"
-                  onChange={(e) => updateCommentField('body', e.target.value)}
-                ></Textarea>
-              </ModalBody>
-              <ModalFooter>
-                <Button type="submit" bg="#66CD00" mr={3}>
-                  Send it
-                </Button>
-                <Button onClick={onClose} variant="ghost">
-                  Cancel
-                </Button>
-              </ModalFooter>
-            </form>
-          </ModalContent>
-        </Modal>
-        <Modal size="xl" isOpen={isSecondOpen} onClose={onClose} isCentered>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>
-              <Flex flexDirection="column">
-                <Box>
-                  <Text>ESPN Just Gave Justin Fields Even More Reasons To Crush 2022</Text>
-                </Box>
-                <Box>
-                  <Text fontSize="sm" mt="3">
-                    Aug 13th, 2019, 10:08 AM
-                  </Text>
-                </Box>
-              </Flex>
-            </ModalHeader>
-            <ModalCloseButton />
-            <form onSubmit={handleAddSubComment}>
-              <ModalBody>
-                <Textarea
-                  value={formSubComment.body}
-                  height={250}
-                  placeholder="Type comment here"
-                  fontFamily="Poppins"
-                  fontSize="20px"
-                  onChange={(e) => updateSubCommentField('body', e.target.value)}
-                ></Textarea>
-              </ModalBody>
-              <ModalFooter>
-                <Button type="submit" bg="#66CD00" mr={3}>
-                  Send it
-                </Button>
-                <Button onClick={onClose} variant="ghost">
-                  Cancel
-                </Button>
-              </ModalFooter>
-            </form>
-          </ModalContent>
-        </Modal>
-      </div>
+                            </Flex>
+                          )
+                        })}
+                      </Box>
+                    )
+                  })}
+                </AccordionPanel>
+              </AccordionItem>
+            )
+          })}
+      </Accordion>
+      <Modal size="xl" isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Flex flexDirection="column">
+              <Box>
+                <Text>ESPN Just Gave Justin Fields Even More Reasons To Crush 2022</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" mt="3">
+                  Aug 13th, 2019, 10:08 AM
+                </Text>
+              </Box>
+            </Flex>
+          </ModalHeader>
+          <ModalCloseButton />
+          <form onSubmit={handleAddComment}>
+            <ModalBody>
+              <Textarea
+                value={formComment.body}
+                height={250}
+                placeholder="Type comment here"
+                fontFamily="Poppins"
+                fontSize="20px"
+                onChange={(e) => updateCommentField('body', e.target.value)}
+              ></Textarea>
+            </ModalBody>
+            <ModalFooter>
+              <Button type="submit" bg="#66CD00" mr={3}>
+                Send it
+              </Button>
+              <Button onClick={onClose} variant="ghost">
+                Cancel
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+      <Modal size="xl" isOpen={isSecondOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Flex flexDirection="column">
+              <Box>
+                <Text>ESPN Just Gave Justin Fields Even More Reasons To Crush 2022</Text>
+              </Box>
+              <Box>
+                <Text fontSize="sm" mt="3">
+                  Aug 13th, 2019, 10:08 AM
+                </Text>
+              </Box>
+            </Flex>
+          </ModalHeader>
+          <ModalCloseButton />
+          <form onSubmit={handleAddSubComment}>
+            <ModalBody>
+              <Textarea
+                value={formSubComment.body}
+                height={250}
+                placeholder="Type comment here"
+                fontFamily="Poppins"
+                fontSize="20px"
+                onChange={(e) => updateSubCommentField('body', e.target.value)}
+              ></Textarea>
+            </ModalBody>
+            <ModalFooter>
+              <Button type="submit" bg="#66CD00" mr={3}>
+                Send it
+              </Button>
+              <Button onClick={onClose} variant="ghost">
+                Cancel
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
